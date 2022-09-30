2e96c1d5672531eaa83373c9bc90aacfac7667c5
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-09-30 12:33:24 +0900

    Time: 7482 ms (23.29%), Space: 52.8 MB (61.56%) - LeetHub

---
+++ 204-count-primes/204-count-primes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 1 :
+            return 0
+        
+        primes = [True] * n
+        primes[0] = primes[1] = False
+        
+        for i in range(2, int(n ** (1/2))+1) :
+            if primes[i] :
+                for j in range(i*2, n, i) :
+                    primes[j] = False
+        
+        
+        return primes.count(True)

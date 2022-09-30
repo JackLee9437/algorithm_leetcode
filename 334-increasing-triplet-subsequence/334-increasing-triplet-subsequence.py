257317e7320b8f858acd44444ee494c386a32d86
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-09-30 13:07:05 +0900

    Time: 1029 ms (46.24%), Space: 27.1 MB (5.16%) - LeetHub

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        accMin = nums[:]
+        accMax = nums[:]
+        
+        for i in range(1, len(accMin)) :
+            accMin[i] = min(accMin[i], accMin[i-1])
+        for i in range(len(accMax)-2, -1, -1) :
+            accMax[i] = max(accMax[i], accMax[i+1])
+            
+        for i in range(1, len(nums)-1) :
+            if accMin[i-1] < nums[i] < accMax[i+1] :
+                return True
+        return False

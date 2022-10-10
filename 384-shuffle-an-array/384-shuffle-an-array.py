8904070a12964da498e969b81ecc2a559213079e
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-10-10 12:22:17 +0900

    Time: 424 ms (18.97%), Space: 17.6 MB (21.92%) - LeetHub

--- 384-shuffle-an-array/384-shuffle-an-array.py
+++ 384-shuffle-an-array/384-shuffle-an-array.py
@@ -4,14 +4,13 @@ class Solution:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        self.origin = nums
+        self.origin = nums[:]
 
     def reset(self) -> List[int]:
         self.nums = self.origin[:]
         return self.nums
         
     def shuffle(self) -> List[int]:
-        self.nums = self.origin[:]
         shuffle(self.nums)
         return self.nums
         

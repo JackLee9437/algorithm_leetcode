d90ac0dfe49f78b10308f808d973fa81af5f5676
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-10-17 14:19:38 +0900

    Time: 3197 ms (38.07%), Space: 27.1 MB (81.25%) - LeetHub

--- 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.py
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.py
@@ -1,15 +1,12 @@
 class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
-        rst = [nums[0]]
-        i = 1
-        while i < len(nums) - k + len(rst) :
-            while rst and rst[-1] > nums[i] and len(nums)-i-1 >= k-len(rst) :
+        rst = []
+        poplimit = len(nums)-k
+        for i in range(len(nums)) :
+            while poplimit > 0 and rst and rst[-1] > nums[i] :
                 rst.pop()
-            if len(rst) < k :
-                rst.append(nums[i])
+                poplimit -= 1
+            rst.append(nums[i])
             i += 1
-
-        if len(rst) < k :
-            rst += nums[len(rst)-k:]
         
-        return rst
+        return rst[:k]

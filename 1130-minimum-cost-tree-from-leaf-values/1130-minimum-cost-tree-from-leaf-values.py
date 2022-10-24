5f70dcb63ded9cbde250e9641445ff33198d6e57
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-10-24 12:44:29 +0900

    Time: 81 ms (48.28%), Space: 14 MB (47.45%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
@@ -3,15 +3,13 @@ class Solution:
         if len(arr) == 2 :
             return arr[0] * arr[1]
         
-        dp = [[0 for _ in range(len(arr))] for __ in range(len(arr))]
-        for i in range(len(arr)-1) :
-            dp[i][i+1] = arr[i] * arr[i+1]
+        answer = 0
+        while len(arr) > 1 :
+            target = arr.index(min(arr))
+            if 0 < target < len(arr)-1 :
+                answer += min(arr[target-1], arr[target+1]) * arr[target]
+            else :
+                answer += (arr[1] if target == 0 else arr[target-1]) * arr[target]
+            arr.pop(target)
         
-        for size in range(3, len(arr)+1) :
-            for i in range(len(arr)-size+1) :
-                for j in range(i+size-1, len(arr)) :
-                    dp[i][j] = int(1e9)
-                    for k in range(i, j) :
-                        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + max(arr[i:k+1]) * max(arr[k+1:j+1]))
-                        
-        return dp[0][len(arr)-1]
+        return answer

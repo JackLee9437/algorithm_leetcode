4edab3868a7324d052e98bfdf3a6d0e7ac80d4f4
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-10-25 12:59:02 +0900

    Time: 9573 ms (5.04%), Space: 27.5 MB (97.91%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -1,6 +1,32 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         nums.sort()
+        preSum = nums[:]
+        for i in range(1, len(preSum)) :
+            preSum[i] += preSum[i-1]
+        
+        answer = 1
+        for j in range(1, len(nums)) :
+            left, right = 0, j
+            i = j
+            while left <= right :
+                mid = (left+right) // 2
+                if nums[j] * (j-mid+1) <= preSum[j] - (preSum[mid-1] if mid > 0 else 0) + k :
+                    i = mid
+                    right = mid - 1
+                else :
+                    left = mid + 1
+            answer = max(answer, j-i+1)
+        
+        return answer
+        
+        
+        
+        
+        
+        
+    def maxFrequency_slidingWindow(self, nums: List[int], k: int) -> int:
+        nums.sort()
         
         l = r = 0
         curSum = 0

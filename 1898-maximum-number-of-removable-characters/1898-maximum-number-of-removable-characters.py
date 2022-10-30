7501cb74a92525cc20ea041739053db52d738bfa
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-10-30 21:48:09 +0900

    Time: 3832 ms (88.43%), Space: 30.1 MB (75.88%) - LeetHub

---
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
+        if not len(removable) :
+            return 0
+        
+        def check(k) :
+            removed = set(removable[:k])
+            i = j = 0
+            while i < len(s) and j < len(p) :
+                if i in removed :
+                    i += 1
+                    continue
+                if s[i] == p[j] :
+                    j += 1
+                i += 1
+            return j == len(p)
+        
+        answer = 0
+        left, right = 0, len(removable)
+        while left <= right :
+            mid = (left+right) // 2
+            if len(s) - mid < len(p) :
+                right = mid - 1
+                continue
+            if check(mid) :
+                answer = mid
+                left = mid + 1
+            else :
+                right = mid - 1
+        
+        return answer
